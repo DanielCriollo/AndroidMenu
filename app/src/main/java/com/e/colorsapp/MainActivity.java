e51da2edf80a9fa8bf713e5a49749323aab9bf0b
DanielCriollo <danielcriollo9706@gmail.com>
2019-10-09 15:45:11 -0500

    Agregue las opciones al menuOption

--- app/src/main/java/com/e/colorsapp/MainActivity.java
+++ app/src/main/java/com/e/colorsapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.e.colorsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
@@ -60,5 +64,50 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.colors_menu1,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        switch (item.getItemId()){
+            case R.id.itRed:
+                Toast.makeText(this, "Car", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itGreen:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itBlue:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itYellow:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itTransaparent:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itMagenta:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itsemiTransaparent:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itCyan:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itBrown:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
